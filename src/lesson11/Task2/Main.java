c8f7a1c130667c712b533cdf95625f66eb36e661
Polya404 <polyapolya404@gmail.com>
2022-04-29 23:06:02 +0300

    Initial commit

---
+++ src/lesson11/Task2/Main.java
@@ -0,0 +1,11 @@
+package lesson11.Task2;
+
+public class Main {
+    public static void main(String[] args) {
+        Car opel = new Car();
+        Car honda = new Car(2003);
+        Car daewoo = new Car(2003, 200);
+        Car dodge = new Car(1998, 290, 30000);
+        Car fiat = new Car(2005, 220, 40000, "Blue");
+    }
+}
---
+++ src/lesson11/Task3/Car.java
@@ -0,0 +1,63 @@
+package lesson11.Task3;
+
+public class Car {
+    private int year;
+    private double speed;
+    private int weight;
+    private String color;
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(double speed) {
+        this.speed = speed;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Car() {
+
+    }
+
+    public Car(int year) {
+        this.year = year;
+    }
+
+    public Car(int year, double speed) {
+        this(year);
+        this.speed = speed;
+    }
+
+    public Car(int year, double speed, int weight) {
+        this(year, speed);
+        this.weight = weight;
+    }
+
+    public Car(int year, double speed, int weight, String color) {
+        this(year, speed, weight);
+        this.color = color;
+    }
+}
